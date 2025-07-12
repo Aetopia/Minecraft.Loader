d8b02282bde120236c10062f6a13ad8ab8bd9e23
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-07-12 15:50:18 +0530

    Use Bedrockix.

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Bedrockix;
+using Bedrockix.Windows;
 
 static class Program
 {
-    static void Main(string[] _) => Game.Launch(_.FirstOrDefault());
+    static void Main(string[] args)
+    {
+        var game = Minecraft.Release;
+
+        if (!game.Installed) { Console.WriteLine("Minecraft: Bedrock Edition isn't installed!"); return; }
+
+        if (!args.Any()) { game.Launch(); return; }
+
+        List<Library> libraries = [];
+        foreach (var arg in args)
+        {
+            Library library = new(arg);
+            if (!library.Valid) { Console.WriteLine($"Found invalid dynamic link library: \"{library.Path}\""); continue; }
+            libraries.Add(library);
+        }
+
+        game.Loader.Launch(libraries);
+    }
 }
